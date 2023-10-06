246851bfba7cef8de0a96c2514c6862b4b3e9313
IsqDantas <isaque.dantas@escolar.ifrn.edu.br>
2023-10-05 22:03:11 -0300

    Reformatted code

--- functions.py
+++ functions.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import plotly.express as px
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
@@ -22,16 +21,39 @@ def make_histogram_graphic(data_base_path):
     figure = px.histogram(avg, title="model's accuracy", x='accuracy', y='model', color='model')
     figure.show()
     print(avg)
-    
+
+
+def make_table(table_path):
+    df = pd.read_csv(table_path)
+    external_stylesheets = ['assets/css/style.css']
+    app = Dash(__name__, external_stylesheets=external_stylesheets)
+    app.layout = html.Div([
+        html.Div(children="Model test", style={'fontSize': '24px', 'textAlign': 'center'}),
+        dash_table.DataTable(data=df.to_dict('records'),
+                             style_cell={'textAlign': 'center'},
+                             page_size=15,
+                             style_data={
+                                 'backgroundColor': 'rgb(30, 30, 30)',
+                                 'fontSize': '18px',
+                                 'width': '50px'
+
+                             },
+                             style_header={
+                                 'backgroundColor': 'rgb(50, 50, 50)',
+                                 'fontWeight': 'bold',
+                                 'border': '1px solid white'
+                             })
+    ])
+
+    app.run(debug=True)
+
+
 def export_comparing_table(train_table, model_path, number_of_rows, table_path):
     model = joblib.load(model_path)
     table = pd.read_csv(train_table)
 
     x, y = separate_table(table, number_of_rows)
     prediction = model.predict(x)
-
-    print(x)
-    print(x.columns)
 
     export_table = {
         'Amount': x['Amount'],
@@ -41,7 +63,8 @@ def export_comparing_table(train_table, model_path, number_of_rows, table_path):
 
     export_table = pd.DataFrame(export_table)
     export_table.to_csv(table_path, index=False)
-    
+
+
 def export_train_table(accuracies, model_names, filename):
     while True:
         try:
@@ -129,34 +152,12 @@ def train_all_models(data_base_path, accuracy_path, number_of_rows, number_of_tr
 
 def main():
     # make_histogram_graphic('score-100-trains-40000-db-rows.csv')
-    export_comparing_table('creditcard_2023.csv',
-                           'trained_model.joblib',
-                           15,
-                           'comparing-model-results.csv')
+    # export_comparing_table('creditcard_2023.csv',
+    #                        'trained_model.joblib',
+    #                        15,
+    #                        'comparing-model-results.csv')
+    make_table('comparing-model-results.csv')
 
-def make_table(table_path):
-    df = pd.read_csv(table_path)
-    external_stylesheets = ['assets/css/style.css']
-    app = Dash(__name__, external_stylesheets=external_stylesheets)
-    app.layout = html.Div([
-        html.Div(children="Model test", style={'fontSize': '24px', 'textAlign': 'center'}),
-        dash_table.DataTable(data=df.to_dict('records'),
-                             style_cell={'textAlign': 'center'},
-                             page_size=15,
-                             style_data={
-                                 'backgroundColor': 'rgb(30, 30, 30)',
-                                 'fontSize': '18px',
-                                 'width': '50px'
-                            
-                             },
-                             style_header={
-                                 'backgroundColor': 'rgb(50, 50, 50)',
-                                 'fontWeight': 'bold',
-                                 'border': '1px solid white'
-                             })
-    ])
-    
-    app.run(debug=True)
-    
+
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -11,12 +11,12 @@ import joblib
 
 def main():
     output_file = 'score-test.csv'
-    # train_all_models('creditcard_2023.csv',
-    #                  output_file,
-    #                  30_000,
-    #                  5)
+    train_all_models('creditcard_2023.csv',
+                     output_file,
+                     10_000,
+                     2)
 
-    # make_histogram_graphic(output_file)
+    make_histogram_graphic(output_file)
 
     output_file = pd.read_csv('creditcard_2023.csv')
 
@@ -26,12 +26,14 @@ def main():
 
     print('Exporting last model')
     joblib.dump(random_forest_model, 'RandomForest-40000.joblib')
-    joblib.dump(perceptron_model, 'Perceptron-40000.joblib')
     print('The model RandomForestClassifier was exported successfully.')
-    print('The model Perceptron was exported successfully.')
-    display(pd.read_csv("comparing-model-results.csv"))
 
-make_table('comparing-model-results.csv')
+    joblib.dump(perceptron_model, 'Perceptron-40000.joblib')
+    print('The model Perceptron was exported successfully.')
+
+    display(pd.read_csv("comparing-model-results.csv"))
+    make_table('comparing-model-results.csv')
+
 
 if __name__ == '__main__':
     main()
